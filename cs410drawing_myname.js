3fb0bd5114022a6fc2aa81d3281f2df87d8b6001
tyru5 <tyrusm@hotmail.com>
2016-09-18 17:07:33 -0600

    code format

--- cs410drawing_myname.js
+++ cs410drawing_myname.js
@@ -1,12 +1,11 @@
 
 function drawIt() {
-	dboard = document.getElementById("dboard");
-	cntxt  = dboard.getContext('2d');
+	cntxt = document.getElementById("dboard").getContext('2d');
 	w = dboard.width;
 	h = dboard.height;
 	cntxt.save();
 	cntxt.setTransform(h, 0.0, 0.0, -h, 0.0, h);
-	cntxt.lineWidth=0.01; 
+	cntxt.lineWidth=0.01;
 	cntxt.rect(0.2,0.2,1.0,0.6);
 	cntxt.stroke();
 	cntxt.restore();
