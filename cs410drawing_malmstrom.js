3ad38132f0ab7944cd7f079d73d892d4336d71d1
Tyrus Malmstrom <tyrusm@hotmail.com>
2016-09-19 13:50:58 -0600

    making prog

--- cs410drawing_malmstrom.js
+++ cs410drawing_malmstrom.js
@@ -1,18 +1,78 @@
 
-function drawIt() {
-	cntxt = document.getElementById("dboard").getContext('2d');
+// Tyrus Malmstrom
+// cs 410
+// JavaScript for Project 2.
+
+
+function init() {
+	dboard = document.getElementById("dboard");
+	cntxt = dboard.getContext('2d');
 	var board_width = dboard.width;
 	console.log("The width of the board is: " + board_width);
 	var board_height = dboard.height;
 	console.log("The height of the board is: " + board_height);
+
 	cntxt.save();
 	cntxt.setTransform(board_height, 0.0, 0.0, -board_height, 0.0, board_height);
 	cntxt.lineWidth=0.01;
-	cntxt.rect(0.2,0.2,1.0,0.6);
-	cntxt.stroke();
 	cntxt.restore();
+
+	draw_css_rectangle();
 }
 
-function init() {
-	drawIt();
+// All my code: (9/19/16)
+
+// 'class' for creating a rectangle;
+function make_rectangle(x, y, width, height, fillColor, lineColor){
+	this.x = x;
+	this.y = y;
+	this.width = width;
+	this.height = height;
+	this.fillColor = fillColor;
+	this.lineColor = lineColor;
 }
+
+function draw_css_rectangle(){
+
+	for(var i = 0; i < 32; i++){
+		draw_rectangle();
+		cntxt.transform(0.66, 0.0, 0.0, 0.66, 0.33, 0.33);
+	}
+
+}
+
+function draw_rectangle(){
+	cntxt.fillStyle = "DarkGreen";
+	cntxt.strokeStyle = "DarkGrey";
+	cntxt.rect(0, 0, 0.5, 0.5);
+	cntxt.fill();
+	cntxt.stroke();
+}
+
+
+
+
+// function drawIt() {
+// 	for (i = 0; i < 32; i++) {
+//        drawBox();
+// 	   cntxt.transform(0.66, 0.0, 0.0, 0.66, 0.33, 0.33);
+// 	}
+// }
+//
+// function drawBox() {
+// 	cntxt.fillStyle   = "DarkGreen";
+// 	cntxt.strokeStyle = "DarkGrey";
+// 	cntxt.rect(0, 0, 0.5, 0.5);
+//     cntxt.fill();
+//     cntxt.stroke();
+// }
+//
+// function init() {
+// 	dboard = document.getElementById("dboard");
+// 	cntxt  = dboard.getContext('2d');
+// 	w = dboard.width;
+// 	h = dboard.height;
+// 	cntxt.setTransform(w, 0.0, 0.0, -h, 0.0, h);
+// 	cntxt.lineWidth=0.01;
+// 	drawIt();
+// }
