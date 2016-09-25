94380dabafa68b9fceb1796478d26fd2efb0a8b5
tyru5 <tyrusm@hotmail.com>
2016-09-25 17:22:01 -0600

    animation N' stuff

--- cs410drawing_malmstrom.js
+++ cs410drawing_malmstrom.js
@@ -3,91 +3,97 @@
 // JavaScript for Project 2.
 
 // All my code: (9/19/16)
+
+// global canvas context:
 var context;
 
+// INSTANCE VARIABLES FOR CIRCLE ANIMATION:
+var x = 0.0;
+var y = 0.025;
 var dx = 0.00095;
 var dy = 0.00025;
-var x = 0.0;
-var y = 0.025;;
 var radius = 0.05;
+var opacity = 1;
+// END ANIMATION VARIABLES.
+
+// VARIABLES FOR superNova
+var cx = 0.0;
+var cy = 1.0;
+var cradius = 0.025;
+// END SUPERNOVA VARIABLES
 
 function init() {
+    // initialize canvas and board:
     context = document.getElementById("dboard").getContext('2d');
     var board_width = dboard.width;
-    console.log("The width of the board is: " + board_width);
     var board_height = dboard.height;
-    console.log("The height of the board is: " + board_height);
     context.setTransform(board_height, 0.0, 0.0, -board_height, 0.0, board_height);
     context.lineWidth = 0.004;
-    setInterval(drawBouncingBall, 10);
-    // drawThings();
+    drawThings();
 }
 
 function drawThings() {
-    drawCircle();
-    drawCss_rect();
+    // draw some other things here main:
+    superNova();
+    square();
+    setInterval(slidingBall, 10);
 }
 
-function drawCircle() {
+// function that retruns a random color: (sets it using rgba() )
+function arbitraryColor() {
+    var red = Math.floor(Math.random() * 255);
+    var green = Math.floor(Math.random() * 255);
+    var blue = Math.floor(Math.random() * 255);
+    var aplpha = Math.random();
+    var color = "rgba(" + red + "," + green + "," + blue + "," + aplpha + ")";
+    return color;
+}
 
-    var centerX1 = 1;
-    var centerY1 = 0.5;
-    var radius1 = 0.05;
 
-    var centerX2 = 1;
-    var centerY2 = 0.25;
-    var radius2 = 0.05;
-
+function superNova() {
     context.beginPath();
-    context.fillStyle = "Green";
-    context.arc(centerX1, centerY1, radius1, 0 * Math.PI, 2 * Math.PI);
+    context.fillStyle = arbitraryColor();
+    context.arc(cx, cy, 0.5, 0, 2 * Math.PI);
     context.fill();
-    context.stroke();
-    context.closePath();
-
-    context.beginPath();
-    context.fillStyle = "Yellow";
-    context.arc(centerX2, centerY2, radius2, 0 * Math.PI, 2 * Math.PI);
-    context.fill();
-    context.stroke();
     context.closePath();
 }
 
-function draw_rectangle() {
-
-    var x = 0.0;
-    var y = 0.0;
-    var width = 0.25;
-    var height = 0.25;
-
-    context.beginPath();
-    context.rect(x, y, width, height);
-    context.fillStyle = "Purple";
-    context.strokeStyle = "Black";
-    context.fill();
-    context.stroke();
-
-}
-
-function drawCss_rect() {
-    for (var i = 0; i < 32; i++) {
-        draw_rectangle();
-        context.transform(0.66, 0.0, 0.0, 0.66, 0.25, 0.33);
+var commands = {
+    explosion: function() {
+        setInterval(superNova, 10);
     }
+};
+
+function square(){
+
+  context.fillStyle = "Pink";
+  context.fillRect(0.75, 0.5, 0.25,0.25);
+
+
 }
 
-
-function drawBouncingBall() {
-    context.clearRect(0, 0, 1.5, 1.0);
+function slidingBall() {
+    context.clearRect(0.0, 0.0, 0.855555500000005, 0.26555555000000019); // yeah yeah i know, this is kinda bad haha
     context.beginPath();
-    context.fillStyle = "Red";
-    context.arc(x, y, 0.05, 0, Math.PI * 2, true);
+    context.fillStyle = "rgba(255, 0 ,0 ," + opacity + ")";
+    context.arc(x, y, radius, 0, Math.PI * 2, true);
     context.fill();
     x += dx;
     y += dy;
+    // console.log("x = " + x);
+    // console.log("y = " + y);
+    if (x == 0.816050000000005 && y == 0.23975000000000019) {
+        dx = 0.0;
+        dy = 0.0;
+        opacity -= 0.00055;
+    } else {
+        // update radius and opacity of ball:
+        radius -= 0.00004;
+    }
 
 }
 
+// adding event handler:
 $(document).ready(function() {
     $('#dboard').click(function() {
         // alert("I am clicking man!");
@@ -95,7 +101,7 @@ $(document).ready(function() {
         y = 0.025;
         dx = 0.00095;
         dy = 0.00025;
-        context.clearRect(0,0,1.5,1);
+        radius = 0.05;
+        opacity = 1;
     });
-
 });
